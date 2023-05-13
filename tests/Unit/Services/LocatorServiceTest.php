c1be1b8011b920f7c2c49e8e00a080be01df4c72
Gabriel Vitor <git624886@gmail.com>
2023-05-13 10:49:42 -0300

    feat: tests - faltando mb_string

---
+++ tests/Unit/Services/LocatorServiceTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use LearningPest\Models\Rent\ModelCar;
+use LearningPest\Models\Users\ModelUser;
+use LearningPest\Repositories\DaoUsers;
+use LearningPest\Service\LocatorService;
+
+beforeEach(function () {
+  $this->locator = new LocatorService;
+
+  $this->car = new ModelCar('Fuscão Rosa', 10.000);
+});
+
+it('Mesmo usuário não pode fazer uma nova locação tendo uma já ativa', function ($ana, $pedro) {
+  $this->locator->createRent($this->car, $pedro);
+
+  $this->locator->createRent($this->car, $ana);
+  $this->locator->createRent($this->car, $ana);
+})->with('mockUsers')
+  ->throws(Exception::class, 'Já possui uma locação ativa');
+
+it('Retorno do array deve estar na ordem Ana e Francisco ao final da locação', function ($ana, $pedro) {
+  $this->locator->createRent($this->car, $ana);
+  $this->locator->createRent($this->car, $pedro);
+
+  $rents = $this->locator->getRents();
+  expect($rents)->toHaveCount(2);
+})->with('mockUsers');
+
+dataset('mockUsers', function () {
+  $ana = new DaoUsers('Ana');
+  $pedro = new DaoUsers('Pedro');
+
+  $ana = new ModelUser($ana->getMockData());
+  $pedro = new ModelUser($pedro->getMockData());
+
+  return [
+    [$ana],
+    [$pedro]
+  ];
+});
