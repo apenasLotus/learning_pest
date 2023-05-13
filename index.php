9e45d13f5346559012e0b2910d7bf317ca74854e
Gabriel Vitor <git624886@gmail.com>
2023-05-13 10:49:26 -0300

    script: composer-tests-php roda o pest

--- index.php
+++ index.php
@@ -7,10 +7,19 @@ use LearningPest\Service\LocatorService;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$daoData = new DaoUsers;
-$franchiesco = new ModelUser($daoData->getMockData());
+$ana = new DaoUsers('Ana');
+$ana = new ModelUser($ana->getMockData());
 
-$fuscaRosa = new ModelCar('Fusca Rosa', 12.000);
+$francisco = new DaoUsers('Francisco');
+$francisco = new ModelUser($francisco->getMockData());
+
+$car = new ModelCar('Fusca Rosa', 12.000);
 
 $locator = new LocatorService;
-$locator->createRent($fuscaRosa, $franchiesco);
+$locator->createRent($car, $ana);
+$locator->createRent($car, $francisco);
+
+echo '<pre>';
+print_r($locator->getRents());
+echo '</pre>';
+exit;
